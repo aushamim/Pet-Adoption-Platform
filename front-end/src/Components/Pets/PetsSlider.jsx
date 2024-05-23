b2982059d708bfe7efe385ab2cd6d766638f96aa
aushamim <au.shamim0@gmail.com>
2024-05-23 20:10:49 +0600

    [Bugfix]

--- front-end/src/Components/Pets/PetsSlider.jsx
+++ front-end/src/Components/Pets/PetsSlider.jsx
@@ -77,7 +77,7 @@ const PetsSlider = () => {
                               </span>
                             ) : (
                               <span className="bg-yellow-200 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">
-                                Pending
+                                Available
                               </span>
                             )}
                           </td>
